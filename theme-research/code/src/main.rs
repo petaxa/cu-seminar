18871d3627723b2483ca551822558fbd0ca56cc5
tempestif <soccer.i_y@icloud.com>
2025-07-06 07:11:56 +0900

    実行時間計測

--- theme-research/code/src/main.rs
+++ theme-research/code/src/main.rs
@@ -1,10 +1,13 @@
 mod flatten_ast;
 mod pure_ast;
 mod utils;
+use std::time::Instant;
 
 fn main() {
     let source_text = "if(condition) { foo(); }";
+
     pure_bfs(source_text);
+
     flatten_bfs(source_text);
 }
 
@@ -15,7 +18,9 @@ fn pure_bfs(source_text: &'static str) {
     let json_asts = serde_json::to_string_pretty(&asts[0]).unwrap();
     println!("{}", json_asts);
 
+    let pure_fnc_time = Instant::now();
     pure_ast::bfs(pure_ast::QueueItem::Multiple(asts));
+    println!("Pure AST 実行時間: {:?}", pure_fnc_time.elapsed());
 }
 
 fn flatten_bfs(source_text: &'static str) {
@@ -25,5 +30,7 @@ fn flatten_bfs(source_text: &'static str) {
     let json_asts = serde_json::to_string_pretty(&asts[0]).unwrap();
     println!("{}", json_asts);
 
+    let flatten_fnc_time = Instant::now();
     flatten_ast::bfs(asts);
+    println!("Flatten AST 実行時間: {:?}", flatten_fnc_time.elapsed());
 }
