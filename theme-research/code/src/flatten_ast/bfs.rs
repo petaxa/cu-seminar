13ab89b8a579a7dfba6724cc8b02f634440571b4
tempestif <soccer.i_y@icloud.com>
2025-07-13 05:25:50 +0900

    fix

--- theme-research/code/src/flatten_ast/bfs.rs
+++ theme-research/code/src/flatten_ast/bfs.rs
@@ -35,30 +35,3 @@ pub fn bfs(ast: &Ast, _log_queue: &mut Vec<String>) {
         }
     }
 }
-
-fn process_node(ast: Ast, _log_queue: &mut Vec<String>) {
-    let nodes = ast.nodes;
-    let mut q: VecDeque<usize> = VecDeque::new();
-
-    let root_child = nodes[ROOT_NODE_INDEX * NODE_ELE_COUNT + CHILD_COORD];
-    if root_child != 0 {
-        q.push_back(root_child);
-    }
-    let root_next = nodes[ROOT_NODE_INDEX * NODE_ELE_COUNT + NEXT_COORD];
-    if root_next != 0 {
-        q.push_back(root_next);
-    }
-
-    while let Some(index) = q.pop_front() {
-        let base = index * NODE_ELE_COUNT;
-        let child = nodes[base + CHILD_COORD];
-        if child != 0 {
-            q.push_back(child);
-        }
-
-        let next = nodes[base + NEXT_COORD];
-        if next != 0 {
-            q.push_back(next);
-        }
-    }
-}
